feafbbb4d9be4ea99a56f216daeb48c6f64d108b
kudousyota <fko2547040@stu.o-hara.ac.jp>
2026-05-08 12:13:53 +0900

    プロジェクト追加

---
+++ SummerGame/SceneMain.h
@@ -0,0 +1,17 @@
+#pragma once
+class SceneMain
+{
+public:
+	SceneMain();
+	~SceneMain();
+
+	void Init();
+	void Update();
+	void Draw();
+private:
+	void DrawGrid();
+
+private:
+	int m_frameCount;
+};
+
