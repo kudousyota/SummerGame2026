f7d43783409cf155af469052e8000dead8e69f82
kudousyota <fko2547040@stu.o-hara.ac.jp>
2026-08-09 01:35:39 +0900

    Scoreを少し改造

--- SummerGame/Score.cpp
+++ SummerGame/Score.cpp
@@ -1,18 +1,82 @@
 #include "Score.h"
 #include <algorithm>
 
+namespace
+{
+	//スコアリセット
+	constexpr int kScoreReset = 0;
+}
+
+Score::Score():
+	m_enemyScore(0),
+	m_timeScore(0),
+	m_witchTimeScore(0),
+	m_noDamageScore(0),
+	m_totalScore(0),
+	m_witchTimeCount(0),
+	m_clearTime(0.0f),
+	m_isNoDamage(true)
+{
+
+}
+
+Score::~Score()
+{
+}
+
+void Score::Init()
+{
+	//スコアを初期化
+	m_enemyScore = kScoreReset;
+	m_timeScore = kScoreReset;
+	m_witchTimeScore = kScoreReset;
+	m_noDamageScore = kScoreReset;
+	m_totalScore = kScoreReset;
+
+	m_witchTimeCount = kScoreReset;
+
+	m_isNoDamage = true;
+
+	m_clearTime = kScoreReset;
+}
+
 void Score::AddEnemyScore(int score)
 {
+	m_enemyScore += score;
 }
 
 void Score::SetClearTime(float time)
 {
+	m_clearTime = time;
+}
+
+void Score::AddWitchTimeCount()
+{
+	m_witchTimeCount++;
+}
+
+void Score::OnNoDamage()
+{
+	m_isNoDamage = false;
 }
 
 void Score::Calculate()
 {
 	//3分以内ならボーナス加算
-	m_timeScore = std::max(0, 6000 - static_cast<int>(m_clearTime * 100));
+	m_timeScore = std::max(0, 18000 - static_cast<int>(m_clearTime * 100.0f));
 
-	m_totalScore = m_enemyScore + m_timeScore;
+	//ウィッチタイム発動回数に応じてボーナス加算
+	m_witchTimeScore = m_witchTimeCount * 1000;
+
+	//ノーダメージならボーナス加算
+	if (m_isNoDamage)
+	{
+		m_noDamageScore = 5000;
+	}
+	else
+	{
+		m_noDamageScore = 0;
+	}
+	//合計スコアを計算
+	m_totalScore = m_enemyScore + m_timeScore + m_witchTimeScore + m_noDamageScore;
 }
--- SummerGame/Score.h
+++ SummerGame/Score.h
@@ -2,26 +2,38 @@
 class Score
 {
 public:
+	Score();
+	~Score();
+
 	void Init();
 	//敵を倒したときのスコアを加算する
 	void AddEnemyScore(int score);
 	void SetClearTime(float time);
+
+	//ウィッチタイム発動回数を加算する
+	void AddWitchTimeCount();
+	//ノーダメージフラグを設定する
+	void OnNoDamage();
 
 	//計算してリザルトシーンで呼ぶ
 	void Calculate();
 
 	//Getter
 	int GetEnemyScore()const { return m_enemyScore; }
-	int GetTimeScore()const { return m_enemyScore; }
-	int GetTotalScore()const { return m_enemyScore; }
-	float GetClearTime()const { return m_enemyScore; }
+	int GetTimeScore()const { return m_timeScore; }
+	int GetWitchTimeScore()const { return m_witchTimeScore; }
+	int GetNoDamageScore()const { return m_noDamageScore; }
+	int GetTotalScore()const { return m_totalScore; }
+	float GetClearTime()const { return m_clearTime; }
 
 private:
 
 	int m_enemyScore;
 	int m_timeScore;
+	int m_witchTimeScore;
+	int m_noDamageScore;
 	int m_totalScore;
-
+	int m_witchTimeCount;
 	float m_clearTime;
+	bool m_isNoDamage;
 };
-
